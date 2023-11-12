35e03fe974b024907e98227d1304f8d1dacd7469
Bartlomiej Mlynarski <bartek.mlynarski18@gmail.com>
2023-11-12 17:05:40 +0100

    added providers

--- src/navigation/Providers.tsx
+++ src/navigation/Providers.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { AuthProvider } from '../store/AuthProvider';
-import AppRoutes from './AppRoutes';
-import { InstrumentProvider } from '../store/InstrumentProvider';
+import React from 'react'
+import { AuthProvider } from '../store/AuthProvider'
+import AppRoutes from './AppRoutes'
+import { InstrumentProvider } from '../store/InstrumentProvider'
+import FollowingContextProvider from '../store/FollowingProvider'
+import FavoritesContextProvider from '../store/FavoritesProvider'
+import { PostsContextProvider } from '../store/PostsProvider'
 
 export default function Providers() {
-  return (
-    <AuthProvider>
-      <InstrumentProvider>
-      <AppRoutes />
-      </InstrumentProvider>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<FollowingContextProvider>
+				<InstrumentProvider>
+        <PostsContextProvider>
+            <FavoritesContextProvider>
+					<AppRoutes />
+          </FavoritesContextProvider>
+          </PostsContextProvider>
+				</InstrumentProvider>
+			</FollowingContextProvider>
+		</AuthProvider>
+	)
 }
