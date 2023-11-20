27b0abbc5fb5cfa1f80dea5d49ba7f2b07f49a2a
Bartlomiej Mlynarski <bartek.mlynarski18@gmail.com>
2023-11-20 20:28:00 +0100

    update packages utilities and notfound view

--- src/views/NotFound/NotFoundView.tsx
+++ src/views/NotFound/NotFoundView.tsx
@@ -1,1 +1,15 @@
-export const NotFoundView = () => <h1>Taki widok nie istnieje</h1>
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { useTheme } from '../../store/themeContext'
+export const NotFoundView = () => {
+	const theme = useTheme()
+
+	return (
+		<Box display='flex' justifyContent='center' alignItems='center' minHeight='100vh'>
+			<Typography variant='h4' component='h1' gutterBottom>
+				This view does not exist
+			</Typography>
+		</Box>
+	)
+}
